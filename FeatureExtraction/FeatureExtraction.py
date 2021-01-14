659db03cc05ae07263df20dfa21c8ce11408e1da
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-01-14 17:06:16 +0200

    Added a function that splits a line vertically into 3 areas and computes some features explained in the fn

--- FeatureExtraction/FeatureExtraction.py
+++ FeatureExtraction/FeatureExtraction.py
@@ -35,9 +35,58 @@ def extract_writer_width(line , f2):
     # If we finish the first function we will divide f2 (from paper) by width
     # to account for small spaces.
 
-    # f3 = f2/f1
+    f3 = f2/f1
 
-    return  f1  #, f3
+    return  f1  , f3
+
+
+
+def extract_base_line(lineImg):
+    # Line image has to be threshed in order to reduce noise
+    _ , threshed = cv2.threshold(lineImg , 200 , 255 ,cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)
+
+    # Image is horizontally projected 
+    projection = np.sum(threshed/np.max(threshed),1)
+
+    #if you check the histogram in Figure 2 in the paper ,
+    # you'll see that the histogram can be approximated as 
+    # a normal distribution that has upper baseline = mu -std
+    # and lower baseline = mu + std
+
+    ProjectionRows = np.arange(0,projection.shape[0])
+
+    mu = np.average(ProjectionRows,weights=projection)
+    std_dev= np.sqrt(np.average(np.square(ProjectionRows - mu),weights=projection))
+
+    
+
+    # Variables are named according to the paper so if you dont understand
+    # feel free to have a look ;)
+    
+    upper_baseline = int(mu - std_dev)
+    lower_baseline = int(mu + std_dev)
+
+
+    ProjNotZero = np.nonzero(projection)
+
+
+    topline = np.min(ProjNotZero)
+    bottomline = np.max(ProjNotZero)
+
+
+
+    f1 = abs(topline - upper_baseline)
+    f2 = abs(upper_baseline - lower_baseline)
+    f3 = abs(lower_baseline - bottomline)
+
+    f4 = f1/f2
+    f5 = f1 / f3
+    f6 = f2/f3
+
+    return np.array([f1,f2,f3,f4,f5,f6])
+
+
+
 
 
 
