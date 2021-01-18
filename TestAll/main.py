6becd54d571ac19d48abc3036ee69f5644b1e52f
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-01-18 08:22:57 +0200

    Finally decided on classifier and num features to use

--- TestAll/main.py
+++ TestAll/main.py
@@ -1,12 +1,8 @@
 from Pipeline import *
-from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
 
 lr = LogisticRegression()
-pipe = Pipeline(r'F:\Test Set Sample',20,lr)
+pipe = Pipeline(r'F:\Pattern_Proj_Dataset\Form\Tests',17,lr)
 
 
-model_coef = pipe.LoopOverDirectories()
-
-for i in model_coef:
-    print(i)
+pipe.LoopOverDirectories()
